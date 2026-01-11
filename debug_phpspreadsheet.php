9a15fb26107c8549bb02c1fdb1eb27f94ecdd501
iamzero205 <chinhface4@gmail.com>
2026-01-11 15:22:27 +0700

    Cleanup: Remove debug and test files

--- debug_phpspreadsheet.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/vendor/autoload.php';
-
-try {
-    $spreadsheet = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
-    echo "SUCCESS: PhpOffice\PhpSpreadsheet\Spreadsheet class found!";
-} catch (\Throwable $e) {
-    echo "FAILURE: " . $e->getMessage();
-}
